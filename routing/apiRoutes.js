d80478a2d7f38490fbca253be9a55f81039761f5
Jack <lijaack@yahoo.com>
2018-12-20 20:48:28 -0800

    function now finds a friend!

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,6 +1,10 @@
 
 var friends = require("../app/data/friends");
 
+var foundFriend;
+var newFriend;
+var lowestDifference = 50;
+
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
@@ -8,9 +12,36 @@ module.exports = function(app) {
       });
 
     app.post("/api/friends", function(req, res) {
+        newFriend = req.body;
+        findFriend()
+        res.json(foundFriend);
         friends.push(req.body);
-        res.json(true);
     });
 
 
+
 };
+
+function findFriend(){
+
+    for(var i = 0; i < friends.length; i++){
+
+        var totalDifference = 0;
+
+        for (var j = 0; j < friends[i].scores.length; j++){
+            
+            var difference = newFriend.scores[j] - friends[i].scores[j]
+            totalDifference += Math.abs(difference);
+        }
+        if (totalDifference < lowestDifference){
+            lowestDifference = totalDifference;
+            foundFriend = friends[i];
+        }
+
+    }
+
+
+
+
+
+}
