ff6c8f9eab8f1d32be99957a3371650ef13299e6
Jack <lijaack@yahoo.com>
2018-12-20 20:09:41 -0800

    fixed routing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsData = [
     {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"jenna coleman",
+    "photo":"https://metrouk2.files.wordpress.com/2017/08/jenna-coleman-doctor-who-main.jpg",
     "scores":[
         5,
         3,
@@ -16,19 +16,19 @@ var friendsData = [
       ]
     },
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            3,
-            1,
-            4,
-            4,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1
+      "name":"Jack",
+      "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmafQYXBv-CM-lX6t4geWKB5kUHXAUUpwem47cAtuckdq1dFXgtg",
+      "scores":[
+          3,
+          1,
+          4,
+          4,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1
           ]
       },
       {
